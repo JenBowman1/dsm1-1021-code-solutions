d203d21be3068b7ebe83db3b7f49d94374338095
JenBowman1 <jennabowman17@gmail.com>
2021-12-16 19:06:05 -0800

    Completed prep example exercise.

---
+++ ds-prep-example/example.py
@@ -0,0 +1,17 @@
+print("""
+  ===============
+  ds-prep-example
+  ===============
+""")
+
+print("I would like to analyze {} to learn/predict {}.\n".format(
+
+  # write what you would like to analyze in the quotes here 👇
+
+  "videos of the great barrier reef",
+
+  # write what you would like to learn from that analysis here 👇
+
+  "the health of an ecosystem"
+
+))
